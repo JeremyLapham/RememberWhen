d28c8a34b0cd0f116be56105608f385822ed05f2
Lerissa Lazar <lerissa.lazar12@gmail.com>
2023-05-03 15:03:16 -0700

    desktopnav update

--- src/Components/DesktopNavComponent/DesktopNav.tsx
+++ src/Components/DesktopNavComponent/DesktopNav.tsx
@@ -1,28 +1,39 @@
 import React from 'react'
 import "./DesktopNav.css";
-import { Navbar, Button, Row, Col } from 'react-bootstrap';
+import { Navbar, Button, Row, Col, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Navlogo from '../../assets/elephantLogo.svg';
 
 export default function DesktopNav() {
     return (
         <Navbar className='desktopNavbar'>
-            <img src={Navlogo} className='logo'></img>
+            <img src={Navlogo} className='navLogo'></img>
             <h1 className='navTitle'>Remember When...</h1>
             <Row className='navRow'>
                 <Col md={2}>
-                    <Button className='navBtn'>Home</Button>
+                    <Nav.Link as={Link} to='/DashBoard'>
+                        <Button className='navBtn'>Home</Button>
+                    </Nav.Link>
                 </Col>
-                <Col md={2}>
-                    <Button className='navBtn'>Setting</Button>
+                <Col md={2} className='borderCol'>
+                    <Nav.Link as={Link} to='/Logout'>
+                        <Button className='navBtn'>Setting</Button>
+                    </Nav.Link>
                 </Col>
                 <Col md={3}>
-                    <Button className='navBtn'>Add Memory</Button>
+                    <Nav.Link as={Link} to='/AddMemory'>
+                        <Button className='navBtn'>Add Memory</Button>
+                    </Nav.Link>
                 </Col>
-                <Col md={3}>
-                    <Button className='navBtn'>Add Folder</Button>
+                <Col md={3} className='borderCol'>
+                    <Nav.Link as={Link} to='/AddFolder'>
+                        <Button className='navBtn'>Add Folder</Button>
+                    </Nav.Link>
                 </Col>
                 <Col md={2}>
-                    <Button className='navBtn'>Memories</Button>
+                    <Nav.Link as={Link} to='/ShownMemory'>
+                        <Button className='navBtn'>Memories</Button>
+                    </Nav.Link>
                 </Col>
             </Row>
         </Navbar>
